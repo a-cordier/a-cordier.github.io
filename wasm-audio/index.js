9dbba3ce5e340974ac3cf73a9839b070d39a58ee
Antoine Cordier <com.acordier@gmail.com>
2020-12-23 17:46:47 +0100

    :rocket: update wasm-audio

--- wasm-audio/index.js
+++ wasm-audio/index.js
@@ -6688,7 +6688,7 @@
               .semiControlID=${MidiControlID.OSC1_SEMI}
               .centControlID=${MidiControlID.OSC1_CENT}
               .cycleControlID=${MidiControlID.OSC1_CYCLE}
-              label="Osc 1"
+              label="Osc. 1"
               .state=${this.state.osc1}
               @change=${this.onOsc1Change}
             ></oscillator-element>
@@ -6702,7 +6702,7 @@
               .semiControlID=${MidiControlID.OSC2_SEMI}
               .centControlID=${MidiControlID.OSC2_CENT}
               .cycleControlID=${MidiControlID.OSC2_CYCLE}
-              label="Osc 2"
+              label="Osc. 2"
               .state=${this.state.osc2}
               @change=${this.onOsc2Change}
             ></oscillator-element>
@@ -6715,7 +6715,7 @@
           <div class="panels-row lower">
             <envelope-element
               .currentLearnerID=${this.currentLearnerID}
-              label="envelope"
+              label="Envelope"
               .state=${this.state.envelope}
               @change=${this.onAmplitudeEnvelopeChange}
             ></envelope-element>
@@ -6723,7 +6723,7 @@
               .currentLearnerID=${this.currentLearnerID}
               .frequencyControlID=${MidiControlID.LFO1_FREQ}
               .modAmountControlID=${MidiControlID.LFO1_MOD}
-              label="lfo 1"
+              label="LFO 1"
               .state=${this.state.lfo1}
               @change=${this.onLfo1Change}
             ></lfo-element>
@@ -6731,7 +6731,7 @@
               .currentLearnerID=${this.currentLearnerID}
               .frequencyControlID=${MidiControlID.LFO2_FREQ}
               .modAmountControlID=${MidiControlID.LFO2_MOD}
-              label="lfo 2"
+              label="LFO 2"
               .state=${this.state.lfo2}
               @change=${this.onLfo2Change}
             ></lfo-element>
